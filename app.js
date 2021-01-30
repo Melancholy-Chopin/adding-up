12a4183deba1a712a3c1a8bbe929fbff01380d67
Kazutaka Saito <kz.bono.1220@gmail.com>
2021-01-30 22:09:16 +0900

    Revert "人口変化率ワーストランキング実装"
    This reverts commit 5529c46c2aa4a2090c3af7c02c2fced74821bbc6.

--- app.js
+++ app.js
@@ -32,14 +32,10 @@ rl.on('close', () => {
         value.change = value.popu15/value.popu10;
     }
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair1[1].change - pair2[1].change;
+        return pair2[1].change - pair1[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value], i) => {
+    const rankingStrings = rankingArray.map(([key, value]) => {
         return(
-            '第' +
-            (i + 1) +
-            '位' +
-            ' ' +
             key +
             ':' +
             value.popu10 +
